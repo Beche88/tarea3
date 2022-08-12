0da4b8bb638c70c3b3e0f26da62f795eefaa5467
Bruce <bruce.cg@outlook.com>
2022-08-12 19:31:49 -0400

    Api test.1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ urllib3.disable_warnings()
 url = "https://192.168.100.60"
 
 
-r = requests.get(url+"/rest/ip/address/*2", auth=(conf.user,conf.clave), verify=False)
+r = requests.get(url+"/rest/ip/address/*3", auth=(conf.user,conf.clave), verify=False)
 print(json.dumps(r.json(), indent=4))
 
 
@@ -22,5 +22,14 @@ else:
 
 #eliminar el registro con una ID específica
 
-r3 = requests.delete(url+"/rest/ip/address/*2", auth=(conf.user,conf.clave), verify=False)
+#r3 = requests.delete(url+"/rest/ip/address/*3", auth=(conf.user,conf.clave), verify=False)
+#print(json.dumps(r3.json(), indent=2))
+
+#########################################################################################3333
+
+
+cabecera = {"content-type": "application/json"}
+body = {"address":"192.168.100.1","duration":"10s"}
+
+r3 = requests.post(url+"/rest/tool/bandwidth-test", auth=(conf.user,conf.clave), headers=cabecera, json=body, verify=False)
 print(json.dumps(r3.json(), indent=2))
